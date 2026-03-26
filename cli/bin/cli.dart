b7764ad448433b562576a580ee05cd3594d48222
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-03-26 19:10:38 +0300

    Приложение с реализованными  командами для проверки версии и получения справки

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,17 @@
+const version = '0.0.1'; // Add this line
+
 void main(List<String> arguments) {
-  print('Hello, Dart');
+ if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); // Change this from 'Hello, Dart!'
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else {
+    printUsage(); // Catch-all for any unrecognized command.
+  }
+}
+
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
 }
