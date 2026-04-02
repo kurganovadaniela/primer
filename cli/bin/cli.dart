3522256c8ed79c18144027d2eeb957e280bd571a
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-04-02 21:49:41 +0300

    Импорт и использвание пакета command_runner в cli.dart. Проведение рефакторинга кода в main

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,20 +1,11 @@
 import 'dart:io'; 
+import 'package:command_runner/command_runner.dart';
 import 'package:http/http.dart' as http;
 const version = '0.0.1'; 
 
-void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-   
-  }
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
   
 }
 
