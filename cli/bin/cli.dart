6b36d103b619b76d7f556ceb6f620a0628961511
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-04-02 21:52:59 +0300

    Импорт и использвание пакета command_runner в cli.dart. Проведение рефакторинга кода в main. Функции printUsage, searchWikipedia, an getWikipediaArticle полностью удалены из cli/bin/cli.dart. Их логика будет переработана и перенесена в пакет command_runner

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -9,42 +9,4 @@ void main(List<String> arguments) async {
   
 }
 
-void searchWikipedia(List<String>? arguments) async { 
-  final String articleTitle;
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    articleTitle = arguments.join(' ');
-  }
 
-  print('Looking up articles about "$articleTitle". Please wait.');
-
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent); 
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-   final url = Uri.https(
-    'en.wikipedia.org',
-    '/api/rest_v1/page/summary/$articleTitle',
-  );
-  final response = await http.get(url); 
-
-  if (response.statusCode == 200) {
-    return response.body; 
-  }
-
- 
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
-}
-void printUsage() { 
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
-  );
-}
