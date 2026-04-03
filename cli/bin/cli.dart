5b764d8a2ff26aee0539b320becbc7c0d3623b78
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-04-03 13:20:45 +0300

    Изменена старая реализация на CommandRunner и HelpCommand в cli.dart - импортирован пакет command_runner, создан экземпляр CommandRunner с добавлением HelpCommand, вызов run с аргументами

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,12 +1,9 @@
-import 'dart:io'; 
 import 'package:command_runner/command_runner.dart';
-import 'package:http/http.dart' as http;
-const version = '0.0.1'; 
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
-  
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
-
 
