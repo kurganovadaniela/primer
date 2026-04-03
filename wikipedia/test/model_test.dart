f1c5f71c538ead0c101fce5354f4c174b26a65fd
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-04-03 19:15:37 +0300

    Добавлен тест для десериализации SearchResults из файла open_search_response.json - проверка чтения JSON файла, преобразования в объект SearchResults и сравнения длины списка результатов с ожидаемым значением

--- wikipedia/test/model_test.dart
+++ wikipedia/test/model_test.dart
@@ -29,5 +29,14 @@ void main() {
       final List<Article> articles = Article.listFromJson(articleMap);
       expect(articles.first.title.toLowerCase(), 'cat');
     });
+     test('deserialize Open Search results example data from json file '
+        'into an SearchResults object', () async {
+      final String resultsString =
+          await File(openSearchResponse).readAsString();
+      final List<Object?> resultsAsList =
+          jsonDecode(resultsString) as List<Object?>;
+      final SearchResults results = SearchResults.fromJson(resultsAsList);
+      expect(results.results.length, greaterThan(1));
+    });
   }); 
 }
