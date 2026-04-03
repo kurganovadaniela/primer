78a00527505f20d29cc1664c01d6449571974ca2
Курганова Даниэла Андреевна <kurganovadan@yandex.ru>
2026-04-03 20:34:35 +0300

    Добавлена в logger.dart логика создания директории logs и файла журнала с уникальным именем на основе даты

--- cli/lib/src/logger.dart
+++ cli/lib/src/logger.dart
@@ -4,9 +4,19 @@ import 'package:logging/logging.dart';
 Logger initFileLogger(String name) {
 
   hierarchicalLoggingEnabled = true;
-
   final logger = Logger(name);
   final now = DateTime.now();
+
+  final scriptFile = File(Platform.script.toFilePath());
+  final projectDir = scriptFile.parent.parent.path;
+
+  final dir = Directory('$projectDir/logs');
+  if (!dir.existsSync()) dir.createSync();
+
+ 
+  final logFile = File(
+    '${dir.path}/${now.year}_${now.month}_${now.day}_$name.txt',
+  );
 
   return logger;
 }
